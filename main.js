5fce96ac80e8f513319333e27287abea913fbf88
Benas Buivydas <buivydas.benas@gmal.com>
2018-10-14 13:45:10 +0100

    made function for computer to pick random from rock paper and scissors

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+function computerSelection(){
+    let computerGuess = getRandomInt(3)
+    if (computerGuess === 0){
+        computerHand = "Rock"
+    }
+    else if (computerGuess === 1){
+        computerHand = "Paper"
+    }
+    else if (computerGuess === 2){
+        computerHand = "Scissors"
+    }
+    return computerHand
+}
